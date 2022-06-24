74ec0cd322c8a4b58b7fe7e9cc634164142df522
Tomas Melone <tomasmelone@hotmail.com>
2022-06-24 22:30:42 +0200

    add examples class routes

--- src/api/routes/example.routes.ts
+++ src/api/routes/example.routes.ts
@@ -1,24 +1,41 @@
 import { body } from 'express-validator';
 import { MadRouter } from 'mad-server'
+import { ExampleFactory } from '../../example/example-factory';
 import exampleController from '../controllers/example.controller';
+import validateRequest from '../middlewares/validateRequest';
 
+//Normal functional controller
 const getExample = {
   path: '/',
   method: 'GET',
   handler: exampleController.example
 }
 
-const postExample = {
-  path: '/',
-  method: 'POST',
-  middlewares: [body('var1').exists(), body('var2').isString()],
-  handler: exampleController.example
+//Class based controllers
+const exampleClassController = new ExampleFactory().getController()
+
+const getClassExample = {
+  path: '/class',
+  method: 'GET',
+  handler: exampleClassController.getExamples
 }
+
+const postClassExample = {
+  path: '/class',
+  method: 'POST',
+  middlewares: [body('field1').exists(), body('field2').isString(), validateRequest],
+  handler: exampleClassController.createExample
+}
+
 
 const router = new MadRouter({
   basePath: '/example',
   name: 'Example',
-  handlers: [getExample]
+  handlers: [
+    getExample, 
+    getClassExample,
+    postClassExample
+  ]
 })
 
 export default router
