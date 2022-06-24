e834b570fafe47ab749759c7de75fd156c8a2c0e
Tomas Melone <tomasmelone@hotmail.com>
2022-06-24 21:57:43 +0200

    ref database connector

--- src/db/connector.ts
+++
@@ -1,9 +0,0 @@
-// import { connect } from 'ODM/ORM';
-
-const options = {
-    //Connection options
-}
-
-export default {
-    // createConnection: async () => connect(options) //Uncomment this line when db connection configured properly
-}
