d280e75f402bb6ff40515aaa5a7653f47420e7dd
Tomas Melone <tomasmelone@hotmail.com>
2022-06-24 21:58:19 +0200

    remove function not used anymore

--- src/utils/helpers-functions.ts
+++ src/utils/helpers-functions.ts
@@ -1,13 +1,3 @@
-import { Request } from 'express';
-
-export const parseIp = (req: Request) => {
-    try {
-        return ((req.headers['x-forwarded-for'] as any) || '').split(',').pop().trim() || req.socket.remoteAddress;
-    } catch (error) {
-        return null;
-    }
-}
-
 export const createCustomError = (err: any, code: number, message?: string) => {
     const error: any = new Error(message || err.message);
     if (err.stack) error.stack = err.stack;        
